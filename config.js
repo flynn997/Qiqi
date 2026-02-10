dc5beeae5edd801957573464eb9bbc9974a572d1
Flynn <nicoflynn557@gmail.com>
2026-02-10 06:23:13 +0530

    delete config

--- config.js
+++
@@ -1,5 +0,0 @@
-export const CONFIG = {
-    GEMINI_KEY: "AIzaSyDDN5vEHvmi8xxecTx3ElBE8UBSz_f0UZ4",
-    ELEVENLABS_KEY: "00fcb1dd55f52602e53145d8826bbda61b417a8fcddc3983126d5eb9dadecef3",
-    VOICE_ID: "vKhbRB1OPHbZx1b510Gf", // Put your custom or default ID here
-};
